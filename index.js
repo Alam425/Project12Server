0e7ae5e42cec10caeba1183e00f75a7e9b0ecae8
Alam425 <body.flexer.627492@gmail.com>
2023-08-23 07:43:49 +0600

    ssd

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-console.log('index');
-console.log('index');
-console.log('index');
-console.log('index');
-console.log('index');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.USE}:${process.env.PASWD}@cluster0.suexuc8.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const classCollection = client.db("classesCollection").collection("classes");
+
+    app.get('/classes', async(req, res) => {
+        const result = classCollection.find().toArray();
+        res.send(result);
+    })
+
+    console.log("Pinged to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get('/', async(req, res) => {
+    res.send('App is running on: ', port)
+})
+
+app.listen(port, () => {
+    console.log('Server is running on :', port);
+})
